19c976b3a5c655ddabb1b94aa64adf85799fd22c
alberthgrande <alberthgrande98@gmail.com>
2026-02-25 09:51:15 +0800

    Done object data types

---
+++ JavaScript/JS Data Types/js_object_types.js
@@ -0,0 +1,32 @@
+{
+  // object
+  const person = { firstName: "Alberth", lastName: "Grande" };
+  console.log(person);
+
+  // array
+  const cars = ["Saab", "Volvo", "BMW"];
+  console.log(cars);
+
+  // date
+  const date = new Date("2026-02-25");
+  console.log(date);
+}
+
+// JavaScript Objects
+{
+  const person = {
+    firstName: "Alberth",
+    lastName: "Grande",
+    age: 27,
+    eyeColor: "brown",
+  };
+  console.log(
+    `${person.firstName} ${person.lastName} is ${person.age} years old and has ${person.eyeColor} eyes.`,
+  );
+}
+
+// JavaScript Arrays
+{
+  const cars = ["Saab", "Volvo", "BMW"];
+  console.log(cars[0]);
+}
