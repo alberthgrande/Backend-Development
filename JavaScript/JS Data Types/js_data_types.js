315e8d6b47b991f02c25451b500b4c3a6e916836
alberthgrande <alberthgrande98@gmail.com>
2026-02-25 09:17:55 +0800

    Done data types

---
+++ JavaScript/JS Data Types/js_data_types.js
@@ -0,0 +1,49 @@
+/*
+
+Type	    Description
+Number	    A number representing a numeric value
+Bigint	    A number representing a large integer
+String	    A text of characters enclosed in quotes
+Boolean	    A data type representing true or false
+Undefined	A variable with no assigned value
+Null	    A value representing object absence
+Symbol	    A unique primitive identifier
+Object	    A collection of key-value pairs of data
+
+Examples:
+// Number
+let length = 16;
+let weight = 7.5;
+
+// BigInt
+let x = 1234567890123456789012345n;
+let y = BigInt(1234567890123456789012345)
+// Strings
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Boolean
+let x = true;
+let y = false;
+
+// Undefined
+let x;
+let y;
+
+// Null
+let x = null;
+let y = null;
+
+// Symbol
+const x = Symbol();
+const y = Symbol();
+
+// Object
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array Object
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date Object
+const date = new Date("2022-03-25");
+*/
