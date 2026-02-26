58317950a6d6cebc5505c31dc8a8f49044353a6f
alberthgrande <alberthgrande98@gmail.com>
2026-02-26 19:09:30 +0800

    Done Js Function Intro

---
+++ JavaScript/03_JS Functions/function_intro.js
@@ -0,0 +1,30 @@
+{
+  function sayHello() {
+    return "Hello, World!";
+  }
+
+  let greeting = sayHello();
+
+  console.log(greeting);
+}
+
+{
+  function multiply(a, c) {
+    return a * c;
+  }
+
+  let result = multiply(5, 5);
+  console.log(result);
+}
+
+{
+  function add(a, c) {
+    return a + c;
+  }
+
+  let sum1 = add(5, 5);
+  let sum2 = add(5, 5);
+
+  console.log(sum1);
+  console.log(sum2);
+}
